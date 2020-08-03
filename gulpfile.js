9812aec2e2829922ab7bacb4fa56f3b453a72acd
maljenkins <53225525+maljenkins@users.noreply.github.com>
2020-08-03 09:31:56 -0400

    Changed Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,35 @@
 const gulp = require("gulp");
 const nunjucksRender = require("gulp-nunjucks-render");
 const data = require("gulp-data");
+const browserSync = require("browser-sync");
+const server = browserSync.create();
 
-gulp.task("default", function () {
-  return gulp
-    .src("src/templates/*.html")
+
+const paths = {
+  html: {
+    src: 'src/templates/**/*'
+  },
+  data: {
+    src: 'src/data/*.json'
+  }
+};
+
+function reload(done) {
+  server.reload();
+  done();
+}
+
+function serve(done) {
+  server.init({
+    server: {
+      baseDir: './dist/'
+    }
+  });
+  done();
+}
+
+function nunjucks(done) {
+  gulp.src("./src/templates/*")
     .pipe(
       data(function () {
         return require("./src/data/global.json");
@@ -12,9 +37,15 @@ gulp.task("default", function () {
     )
     .pipe(
       nunjucksRender({
-        path: ["src/templates/"], // String or Array
+        path: ["./src/templates/"],
       })
     )
-    .pipe(gulp.dest("dist"));
-});
+    .pipe(gulp.dest("./dist/"));
+  done();
+}
+
+const watch = () => gulp.watch([paths.html.src, paths.data.src], gulp.series(nunjucks, reload));
+
+const dev = gulp.series(nunjucks, style, serve, watch);
+exports.default = dev;
 
