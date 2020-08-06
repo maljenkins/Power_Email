8324c1cf3fa3c04d661161727a97957d7a37cee2
maljenkins <53225525+maljenkins@users.noreply.github.com>
2020-08-06 09:50:54 -0400

    Updated gulp file to live render json

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ const data = require("gulp-data");
 const plumber = require("gulp-plumber");
 const browserSync = require("browser-sync");
 const server = browserSync.create();
+const fs = require('fs');
 
 
 const paths = {
@@ -29,14 +30,14 @@ function serve(done) {
   done();
 }
 
+function getData(file) {
+  return JSON.parse(fs.readFileSync(file));
+}
+
 function nunjucks(done) {
   gulp.src("./src/templates/*")
     .pipe(plumber())
-    .pipe(
-      data(function () {
-        return require("./src/data/global.json");
-      })
-    )
+    .pipe(data(getData('./src/data/global.json')))
     .pipe(
       nunjucksRender({
         path: ["./src/templates/"],
@@ -48,6 +49,6 @@ function nunjucks(done) {
 
 const watch = () => gulp.watch([paths.html.src, paths.data.src], gulp.series(nunjucks, reload));
 
-const dev = gulp.series(nunjucks, style, serve, watch);
+const dev = gulp.series(nunjucks, serve, watch);
 exports.default = dev;
 
